afa269eb64ee1082caae8c8f4b163092ee24975f
Juan Pablo <juan_pablo_rojas@hotmail.com>
2024-02-08 11:33:53 -0300

    Se desarrolla requerimiento de autenticacion, se agregan mejoras de encryptacion

--- obj/Debug/net8.0/HomeBankingNetMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeBankingNetMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeBankingNetMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db53df13965c8f02db875c0d4523a362cfd01eb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b96f530f02f77bfa0bef20b26adcb23741fc31a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeBankingNetMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeBankingNetMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
